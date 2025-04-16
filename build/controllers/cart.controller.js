ac63ba198434f3a090a89ab079a3de09bef643b4
Rajesh-zha <Zharazesh89@gmail.com>
2025-04-16 10:56:26 +0545

    --

--- build/controllers/cart.controller.js
+++ build/controllers/cart.controller.js
@@ -13,19 +13,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.removeItemFromCart = exports.clearCart = exports.getCartByUserId = exports.create = void 0;
-const errorhandler_middleware_1 = __importDefault(require("../middlewares/errorhandler.middleware"));
-const product_model_1 = __importDefault(require("../models/product.model"));
 const asyncHandler_util_1 = require("../utils/asyncHandler.util");
+const errorhandler_middleware_1 = __importDefault(require("../middlewares/errorhandler.middleware"));
 const cart_model_1 = require("../models/cart.model");
-const pagination_utils_1 = require("../utils/pagination.utils");
+const product_model_1 = __importDefault(require("../models/product.model"));
 exports.create = (0, asyncHandler_util_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { userId, productId, quantity } = req.body;
+    const { productId, quantity } = req.body;
+    console.log("👊 ~ cart.controller.ts:10 ~ create ~ quantity:", quantity);
+    const userId = req.user._id;
     let cart;
     if (!userId) {
-        throw new errorhandler_middleware_1.default('userId is required', 400);
+        throw new errorhandler_middleware_1.default("userId is required", 400);
     }
     if (!productId) {
-        throw new errorhandler_middleware_1.default('productId is required', 400);
+        throw new errorhandler_middleware_1.default("productId is required", 400);
     }
     cart = yield cart_model_1.Cart.findOne({ user: userId });
     if (!cart) {
@@ -33,94 +34,67 @@ exports.create = (0, asyncHandler_util_1.asyncHandler)((req, res) => __awaiter(v
     }
     const product = yield product_model_1.default.findById(productId);
     if (!product) {
-        throw new errorhandler_middleware_1.default('product not found', 404);
+        throw new errorhandler_middleware_1.default("product not found", 404);
     }
     const existingProduct = cart.items.find((item) => item.product.toString() === productId);
+    console.log("👊 ~ cart.controller.ts:39 ~ create ~ existingProduct:", existingProduct);
     if (existingProduct) {
         existingProduct.quantity += Number(quantity);
-        cart.items.push(existingProduct);
+        // cart.items.push(existingProduct);
     }
     else {
         cart.items.push({ product: productId, quantity });
     }
     yield cart.save();
     res.status(201).json({
-        status: 'success',
+        status: "success",
         success: true,
-        message: 'Product added to cart',
-        data: cart
+        message: "Product added to cart",
+        data: cart,
     });
 }));
 exports.getCartByUserId = (0, asyncHandler_util_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // Extract pagination parameters from the query
-    const { limit, page } = req.query;
-    // Set default values for pagination
-    const currentPage = parseInt(page) || 1;
-    const queryLimit = parseInt(limit) || 10;
-    const skip = (currentPage - 1) * queryLimit;
-    // Get the user ID from the request parameters
-    const userId = req.params.id;
-    // Find the cart for the user
+    const userId = req.params.userId;
     const cart = yield cart_model_1.Cart.findOne({ user: userId })
-        .populate('user', '-password') // Populate the user field (excluding password)
-        .populate({
-        path: 'items.product', // Populate the product field in the items array
-        select: 'name price description', // Select specific fields from the Product model
-    });
-    // If the cart is not found, throw an error
-    if (!cart) {
-        throw new errorhandler_middleware_1.default('Cart not found', 404);
-    }
-    // Get the total number of items in the cart
-    const totalCount = cart.items.length;
-    // Paginate the items array
-    const paginatedItems = cart.items.slice(skip, skip + queryLimit);
-    // Create a new cart object with paginated items
-    const paginatedCart = Object.assign(Object.assign({}, cart.toObject()), { items: paginatedItems });
-    // Calculate pagination metadata
-    const pagination = (0, pagination_utils_1.getPaginationData)(currentPage, queryLimit, totalCount);
-    // Send the response with the paginated cart
+        .populate("user", "-password")
+        .populate("items.product");
     res.status(200).json({
-        status: 'success',
+        status: "success",
         success: true,
-        message: 'Cart fetched successfully',
-        data: {
-            data: paginatedCart, // Paginated cart data
-            pagination, // Pagination metadata
-        },
+        message: "Cart fetched successfully",
+        data: cart,
     });
 }));
 exports.clearCart = (0, asyncHandler_util_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const userId = req.params.userId;
+    const userId = req.user._id;
     const cart = yield cart_model_1.Cart.findOne({ user: userId });
     if (!cart) {
-        throw new errorhandler_middleware_1.default('Cart does not created yet.', 400);
+        throw new errorhandler_middleware_1.default("Cart does not created yet.", 400);
     }
     yield cart_model_1.Cart.findOneAndDelete({ user: userId });
     res.status(200).json({
-        status: 'success',
+        status: "success",
         success: true,
-        message: 'Cart cleared successfully',
-        data: null
+        message: "Cart cleared successfully",
+        data: null,
     });
 }));
 exports.removeItemFromCart = (0, asyncHandler_util_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const productId = req.params.productId;
     const userId = req.user._id;
     if (!productId) {
-        throw new errorhandler_middleware_1.default('productId is required', 400);
+        throw new errorhandler_middleware_1.default("productId is required", 400);
     }
     const cart = yield cart_model_1.Cart.findOne({ user: userId });
     if (!cart) {
-        throw new errorhandler_middleware_1.default('Cart does not created yet.', 400);
+        throw new errorhandler_middleware_1.default("Cart does not created yet.", 400);
     }
-    // cart.items = cart.items.filter((item)=>item.product.toString() !== productId)
     cart.items.pull({ product: productId });
     const updatedCart = yield cart.save();
     res.status(200).json({
-        status: 'success',
+        status: "success",
         success: true,
-        message: 'Cart updated successfully',
-        data: updatedCart
+        message: "Cart updated successfully",
+        data: updatedCart,
     });
 }));
--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,155 +1,120 @@
 import { Request, Response } from "express";
-import CustomError from "../middlewares/errorhandler.middleware";
-import Product from "../models/product.model";
 import { asyncHandler } from "../utils/asyncHandler.util";
+import CustomError from "../middlewares/errorhandler.middleware";
 import { Cart } from "../models/cart.model";
-import { getPaginationData } from "../utils/pagination.utils";
+import Product from "../models/product.model";
 
+export const create = asyncHandler(async (req: Request, res: Response) => {
+	const { productId, quantity } = req.body;
 
- 
+	console.log("👊 ~ cart.controller.ts:10 ~ create ~ quantity:", quantity);
 
+	const userId = req.user._id;
 
+	let cart;
+	if (!userId) {
+		throw new CustomError("userId is required", 400);
+	}
 
-export const create = asyncHandler(async(req:Request,res:Response)=> {
-    const {userId,productId,quantity} = req.body
-     let cart
-    if(!userId) {
-        throw new CustomError('userId is required',400)
-    }
+	if (!productId) {
+		throw new CustomError("productId is required", 400);
+	}
 
-    if(!productId) {
-        throw new CustomError('productId is required',400)
-    }
+	cart = await Cart.findOne({ user: userId });
 
-     cart = await Cart.findOne({user: userId})
+	if (!cart) {
+		cart = new Cart({ user: userId, items: [] });
+	}
 
-    if(!cart) {
-         cart = new Cart({user:userId,items:[]})
-    }
+	const product = await Product.findById(productId);
 
-    const product = await Product.findById(productId)
+	if (!product) {
+		throw new CustomError("product not found", 404);
+	}
 
-    if(!product) {
-        throw new CustomError('product not found',404)
-    }
+	const existingProduct = cart.items.find(
+		(item) => item.product.toString() === productId
+	);
 
-    const existingProduct = cart.items.find((item)=> item.product.toString()===productId)
-    if(existingProduct) {
-        existingProduct.quantity += Number(quantity)
+	console.log(
+		"👊 ~ cart.controller.ts:39 ~ create ~ existingProduct:",
+		existingProduct
+	);
 
-        cart.items.push(existingProduct)
-    } else {
-        cart.items.push({product:productId,quantity})
-    }
+	if (existingProduct) {
+		existingProduct.quantity += Number(quantity);
 
-    
+		// cart.items.push(existingProduct);
+	} else {
+		cart.items.push({ product: productId, quantity });
+	}
 
-    await cart.save()
+	await cart.save();
 
-    res.status(201).json({
-        status:'success',
-        success:true,
-        message: 'Product added to cart',
-        data:cart
-    })
-})
+	res.status(201).json({
+		status: "success",
+		success: true,
+		message: "Product added to cart",
+		data: cart,
+	});
+});
 
-export const getCartByUserId = asyncHandler(async(req:Request,res:Response)=> {
-        // Extract pagination parameters from the query
-        const { limit, page } = req.query;
-    
-        // Set default values for pagination
-        const currentPage = parseInt(page as string) || 1;
-        const queryLimit = parseInt(limit as string) || 10;
-        const skip = (currentPage - 1) * queryLimit;
-    
-        // Get the user ID from the request parameters
-        const userId = req.params.id;
-    
-        // Find the cart for the user
-        const cart = await Cart.findOne({ user: userId })
-            .populate('user', '-password') // Populate the user field (excluding password)
-            .populate({
-                path: 'items.product', // Populate the product field in the items array
-                select: 'name price description', // Select specific fields from the Product model
-            });
-    
-        // If the cart is not found, throw an error
-        if (!cart) {
-            throw new CustomError('Cart not found', 404);
-        }
-    
-        // Get the total number of items in the cart
-        const totalCount = cart.items.length;
-    
-        // Paginate the items array
-        const paginatedItems = cart.items.slice(skip, skip + queryLimit);
-    
-        // Create a new cart object with paginated items
-        const paginatedCart = {
-            ...cart.toObject(), // Convert Mongoose document to a plain object
-            items: paginatedItems, // Replace the items array with the paginated items
-        };
-    
-        // Calculate pagination metadata
-        const pagination = getPaginationData(currentPage, queryLimit, totalCount);
-    
-        // Send the response with the paginated cart
-        res.status(200).json({
-            status: 'success',
-            success: true,
-            message: 'Cart fetched successfully',
-            data: {
-                data: paginatedCart, // Paginated cart data
-                pagination, // Pagination metadata
-            },
-        });
-    });
+export const getCartByUserId = asyncHandler(
+	async (req: Request, res: Response) => {
+		const userId = req.params.userId;
 
-export const clearCart = asyncHandler(async(req:Request,res:Response)=> {
-    const userId = req.params.userId
+		const cart = await Cart.findOne({ user: userId })
+			.populate("user", "-password")
+			.populate("items.product");
 
-    const cart = await Cart.findOne({user:userId})
+		res.status(200).json({
+			status: "success",
+			success: true,
+			message: "Cart fetched successfully",
+			data: cart,
+		});
+	}
+);
 
-    if(!cart) {
-        throw new CustomError('Cart does not created yet.',400)
-    }
+export const clearCart = asyncHandler(async (req: Request, res: Response) => {
+	const userId = req.user._id;
+	const cart = await Cart.findOne({ user: userId });
 
-    await Cart.findOneAndDelete({user:userId});
+	if (!cart) {
+		throw new CustomError("Cart does not created yet.", 400);
+	}
 
-    res.status(200).json({
-        status:'success',
-        success:true,
-        message:'Cart cleared successfully',
-        data:null
-    })
+	await Cart.findOneAndDelete({ user: userId });
 
-})
+	res.status(200).json({
+		status: "success",
+		success: true,
+		message: "Cart cleared successfully",
+		data: null,
+	});
+});
 
+export const removeItemFromCart = asyncHandler(
+	async (req: Request, res: Response) => {
+		const productId = req.params.productId;
+		const userId = req.user._id;
+		if (!productId) {
+			throw new CustomError("productId is required", 400);
+		}
+		const cart = await Cart.findOne({ user: userId });
+		if (!cart) {
+			throw new CustomError("Cart does not created yet.", 400);
+		}
 
-export const removeItemFromCart = asyncHandler(async(req:Request,res:Response)=> {
-    const productId = req.params.productId;
-    const userId = req.user._id
-    if(!productId) {
-        throw new CustomError('productId is required',400)
-    }
-    const cart = await Cart.findOne({user:userId})
+		cart.items.pull({ product: productId });
 
-    if(!cart) {
-        throw new CustomError('Cart does not created yet.',400)
-    }
+		const updatedCart = await cart.save();
 
-    // cart.items = cart.items.filter((item)=>item.product.toString() !== productId)
-
-    cart.items.pull({product:productId})
-    
-    const updatedCart = await cart.save()
-
-    res.status(200).json({
-        status:'success',
-        success:true,
-        message:'Cart updated successfully',
-        data:updatedCart
-    })
-
-})
+		res.status(200).json({
+			status: "success",
+			success: true,
+			message: "Cart updated successfully",
+			data: updatedCart,
+		});
+	}
+);
